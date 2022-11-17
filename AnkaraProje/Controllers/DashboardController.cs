47ff816d04a7ce2eb40f78f69f1af04aeb82ba16
MuratYucedag <MurattYucedag@users.noreply.github.com>
2022-11-17 15:38:24 +0300

    ....

---
+++ AnkaraProje/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AnkaraProje.Models;
+
+namespace AnkaraProje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        DbAnkaraPortfolioEntities db = new DbAnkaraPortfolioEntities();
+        public ActionResult Index()
+        {
+            ViewBag.v = db.TblTestimonial.Count();
+            ViewBag.v2 = db.TblSkill.Count();
+            ViewBag.v3 = db.TblSkill.Where(x => x.Value >= 80).Count();
+            ViewBag.v4 = db.TblSkill.Average(x => x.Value);
+            return View();
+        }
+
+        public PartialViewResult PartialChart()
+        {
+            return PartialView();
+        }
+    }
+}
